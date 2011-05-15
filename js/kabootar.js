78d2e3ad49e45657402eafce5f4e35e43c831ebb
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-05-15 23:16:38 +0530

    Save the data into the riak database and generalized the beaming of data.

--- js/kabootar.js
+++ js/kabootar.js
@@ -1,14 +1,23 @@
-(function() {
-    var host = "http://localhost:8000";
-    var dataList = [{"Page_Load_Start": _page_load_start}, 
-		    {"Page_Load_End": window._page_load_end}];
-    var strData = "";
+var host = "http://localhost:8000";
+
+function recordData(action, value) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", host + "/beam/", false);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    strData = encodeURI('action='+ action + '&value=' + value);
+    xhr.send(strData);
+}
+
+(function() {
+    var dataList = [{"Page_Load_Start": _page_load_start}, 
+		    {"Page_Load_End": window._page_load_end}];
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", host + "/beam_load/", false);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var strData = "";
     for (var i = 0; i < dataList.length; i++) {
 	for (var j in dataList[i]) {
-	    strData += ((i > 0)?"&":"") + encodeURI('action='+ j + '&value=' + dataList[i][j]);
+	    strData += encodeURI((i > 0?"&":"") + 'action='+ j + '&value=' + dataList[i][j]);
 	}
     }
     xhr.send(strData);
--- tests/test_server.js
+++ tests/test_server.js
@@ -9,7 +9,7 @@ var app = express.createServer(
     express.static(path.dirname(__dirname))
 );
 
-app.post("/beam/", function(req, res) {
+app.post("/beam_load/", function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     var actionList = req.body.action;
     var valueList  = req.body.value;
@@ -18,7 +18,25 @@ app.post("/beam/", function(req, res) {
 	return a - b; 
     });
     riak.save("analytics", "Page_Load_" + valueList.sort()[0], 
-	      {"loadTime": (pageLoadTime < 0?-1:1) * pageLoadTime});
+	      {loadTime: (pageLoadTime < 0?-1:1) * pageLoadTime});
+    res.send();
+});
+
+app.options("/beam_load/", function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send();
+});
+
+app.post("/beam/", function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    var action = req.body.action;
+    var value  = req.body.value;
+    riak.save("analytics", action + "_" + value + "_" + (new Date()).getTime(), 
+	      function() { 
+		  var o = new Object;
+		  o[action] = value;
+		  return o;
+	      }());
     res.send();
 });
 
@@ -28,4 +46,3 @@ app.options("/beam/", function(req, res) {
 });
 
 app.listen(8000);
-
