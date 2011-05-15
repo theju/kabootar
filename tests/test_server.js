5c1181ace08f281e5b581ef999b51d6697cfbd8f
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-05-15 22:30:04 +0530

    Added the riak database save code.

--- tests/test_server.js
+++ tests/test_server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var path = require("path");
+var riak = require("riak-js").getClient();
+var us = require("underscore");
 
 var app = express.createServer(
     express.logger(),
@@ -9,12 +11,20 @@ var app = express.createServer(
 
 app.post("/beam/", function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.send(null);
+    var actionList = req.body.action;
+    var valueList  = req.body.value;
+    var tmpList = us.zip(actionList, valueList);
+    var pageLoadTime = us.reduce(valueList, function(a, b) { 
+	return a - b; 
+    });
+    riak.save("analytics", "Page_Load_" + valueList.sort()[0], 
+	      {"loadTime": (pageLoadTime < 0?-1:1) * pageLoadTime});
+    res.send();
 });
 
 app.options("/beam/", function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.send(null);
+    res.send();
 });
 
 app.listen(8000);
